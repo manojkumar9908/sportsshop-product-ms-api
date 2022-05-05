a53fed1e844bb568dd6ed7168d21f111cf4f8eae
manojkumar9908 <manojroyal9908@gmail.com>
2022-05-05 17:47:10 +0530

    add src pom.xml

---
+++ src/main/java/com/sportsshop/SportsshopProductMsApiApplication.java
@@ -0,0 +1,14 @@
+package com.sportsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsshopProductMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsshopProductMsApiApplication.class, args);
+		System.out.println(" product ms is working");
+	}
+
+}
